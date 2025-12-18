6db6aecf900bc92b0bece0ec4b41d35ccfc097b6
digiprem <empire.digiprem@gmail.com>
2025-12-18 04:56:10 +0100

    Snackbar Scaffold

---
+++ core/designsystem/src/commonMain/kotlin/empire/digiprem/com/core/designsystem/layout/ChirpSnackBarScaffold.kt
@@ -0,0 +1,38 @@
+package empire.digiprem.com.core.designsystem.layout
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.union
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ChirpSnackBarScaffold(
+    snackbarHostState: SnackbarHostState,
+    modifier: Modifier=Modifier,
+    content:@Composable ()-> Unit
+) {
+
+    Scaffold(
+        modifier=modifier,
+        contentWindowInsets = WindowInsets.statusBars.union(WindowInsets.displayCutout).union(WindowInsets.ime),
+        snackbarHost = {
+            SnackbarHost(
+            hostState = snackbarHostState,
+                modifier=Modifier.padding(bottom = 24.dp)
+            )
+        }
+    ) { innerPadding->
+        Box(modifier.padding(innerPadding)){
+            content()
+        }
+    }
+}
