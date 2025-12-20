bdc060d16b20514f510e0b3c0b8c39b36b7af909
digiprem <empire.digiprem@gmail.com>
2025-12-20 02:13:47 +0100

    Register API Endpoints

---
+++ core/data/src/commonMain/kotlin/empire/digiprem/com/core/data/auth/KtorAuthService.kt
@@ -0,0 +1,27 @@
+package empire.digiprem.com.core.data.auth
+
+import empire.digiprem.com.core.data.dto.request.RegisterRequest
+import empire.digiprem.com.core.data.networking.post
+import empire.digiprem.com.core.domain.auth.AuthService
+import empire.digiprem.com.core.domain.util.DataError
+import empire.digiprem.com.core.domain.util.EmptyResult
+import io.ktor.client.HttpClient
+
+class KtorAuthService(
+    private val httpClient: HttpClient
+): AuthService{
+    override suspend fun register(
+        email: String,
+        username: String,
+        password: String
+    ): EmptyResult<DataError.Remote> {
+        return httpClient.post(
+            route ="/auth/register",
+            body = RegisterRequest(
+                email=email,
+                username=username,
+                password=password
+            )
+        )
+    }
+}
---
+++ core/domain/src/commonMain/kotlin/empire/digiprem/com/core/domain/auth/AuthService.kt
@@ -0,0 +1,12 @@
+package empire.digiprem.com.core.domain.auth
+
+import empire.digiprem.com.core.domain.util.DataError
+import empire.digiprem.com.core.domain.util.EmptyResult
+
+interface AuthService {
+  suspend  fun register(
+        email:String,
+        username:String,
+        password:String
+    ): EmptyResult<DataError.Remote>
+}
