6c1a348990bd2e46534359bd67331e5e39a2e613
adrien <a.kakmeni@8pusfx.com>
2026-02-17 16:53:37 +0100

    Avatar Photo UI

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -15,6 +15,8 @@ kotlin {
                 implementation(projects.core.presentation)
                 implementation(compose.components.resources)
                 implementation(compose.components.uiToolingPreview)
+                implementation(libs.coil.compose)
+                implementation(libs.coil.network.ktor)
             }
         }
 
---
+++ core/designsystem/src/commonMain/kotlin/empire/digiprem/com/core/designsystem/components/avatar/ChirpAvatarPhoto.kt
@@ -0,0 +1,86 @@
+package empire.digiprem.com.core.designsystem.components.avatar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import empire.digiprem.com.core.designsystem.theme.ChirpTheme
+import empire.digiprem.com.core.designsystem.theme.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+enum class AvatarSize(val dp: Dp) {
+    SMALL(40.dp)
+
+}
+
+
+@Composable
+fun ChirpAvatarPhoto(
+    displayText: String,
+    modifier: Modifier = Modifier,
+    size: AvatarSize = AvatarSize.SMALL,
+    imageUrl: String? = null,
+    onclick: (() -> Unit)? = null,
+    textColor: Color = MaterialTheme.colorScheme.extended.textPlaceholder
+) {
+
+    Box(
+        modifier = modifier
+            .size(size.dp)
+            .clip(CircleShape)
+            .clickable(
+                onClick = {},
+                enabled = onclick != null
+            )
+            .background(MaterialTheme.colorScheme.extended.secondaryFill)
+            .border(
+                width = 2.dp,
+                color = MaterialTheme.colorScheme.outline,
+                shape = CircleShape
+            ),
+        contentAlignment = Alignment.Center
+
+    ) {
+        Text(
+            text = displayText.uppercase(),
+            style = MaterialTheme.typography.titleMedium,
+            color = textColor
+        )
+        AsyncImage(
+            model = imageUrl,
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = Modifier
+                .clip(CircleShape)
+                .matchParentSize()
+        )
+    }
+
+
+}
+
+
+@Preview
+@Composable
+fun ChirpAvatarPhotoPreview(){
+    ChirpTheme {
+        ChirpAvatarPhoto(
+            displayText = "ADmsa",
+
+        )
+
+    }
+}
