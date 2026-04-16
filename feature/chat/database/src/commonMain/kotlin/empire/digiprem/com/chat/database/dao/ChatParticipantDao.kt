84c00983c9336e38a806094c090f08a9df4eee38
adrien <a.kakmeni@8pusfx.com>
2026-04-16 11:30:58 +0100

    Chat Participant DAO

---
+++ feature/chat/database/src/commonMain/kotlin/empire/digiprem/com/chat/database/dao/ChatParticipantDao.kt
@@ -0,0 +1,18 @@
+package empire.digiprem.com.chat.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import empire.digiprem.com.chat.database.entites.ChatParticipantEntity
+
+@Dao
+interface ChatParticipantDao {
+    @Upsert
+    suspend fun upsertParticipant(participant:ChatParticipantEntity)
+
+    @Upsert
+    suspend fun upsertParticipants(participants:List<ChatParticipantEntity>)
+
+    @Query("SELECT * FROM chatparticipantentity")
+    suspend fun getAllParticipants():List<ChatParticipantEntity>
+}
